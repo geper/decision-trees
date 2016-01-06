c9029da218792e6aaee1928a3544ca3945923108
geper <aaalllexey@gmail.com>
2016-01-06 11:52:09 +0300

    sdf
    sdf

--- rega.js
+++ rega.js
@@ -7,98 +7,39 @@ alert("prs::" + prskey + "::set::debug=1");
 alert("prs::" + prskey + "::set::js=1"); //влючение javascript
 alert("prs::" + prskey + "::set::popup=2");
 alert("prs::" + prskey + "::set::popuptab=next");
-
-
-//-----------------------------------Действия на сайте партнера-----------------------------------
-
-
 //выводим информацию о показе первого блока на вкладку Журнал программы тестирования.
 alert("prs::" + prskey + "::debug::Running First Block");
-
-//задаем параметры команд
-
-// var referer=prompt("prs::" + prskey + "::parse::http://<rndt(jethosting.ru!!regjet.ru!!reg.ru!!nic.ru)>"); //список рефереров
-// var cmdname="nav"; //команда переход
-// var cmdtime=parseInt(10)//время показа команды от 20 до 30 секунд.
-// var cmdparam="https://twitter.com/Berder_/status/682145017369079808"; //параметры команды
-
 //список рефереров
-
 var cmdname="nav"; //команда переход
-var cmdtime=parseInt(2)//время показа команды от 20 до 30 секунд.5
-var cmdparam="https://www.facebook.com/mofigbuul/posts/976463425761980"; //параметры команды
+var cmdtime=parseInt(13)//время показа команды от 20 до 30 секунд.5
+var cmdparam="http://twitter.com/dimonddddd"; //параметры команды
 //добавляем команду команду в презентацию. если удачно - команда появится на вкладке "Информация" программы тестирования презентации.
 alert("prs::" + prskey + "::add::" + cmdname + "::" + cmdtime + "::" + cmdparam);
-
-//команда выполнилась
-alert("prs::" + prskey + "::debug::nav");
-
+//########
 var cmdname="click";
-var cmdtime=parseInt(15);
-var cmdparam="a;link;geper.delightfulimageparty.net;"
+var cmdtime=parseInt(17);
+var cmdparam="a;link;t.co;-1"
 alert("prs::" + prskey + "::add::" + cmdname + "::" + cmdtime + "::" + cmdparam); //Клик по случайной ссылки на сайте!
-
-//event
-
-// var cmdname="event";
-// var cmdtime=parseInt(10);
-// var cmdparam="a;text;geper.pythonanywhere.com";
-// alert("prs::" + prskey + "::add::" + cmdname + "::" + cmdtime + "::" + cmdparam); //Клик по случайной ссылки на сайте!
-
-
-
-// var cmdname="nav"; //команда переход
-// var cmdtime=parseInt(prompt("prs::" + prskey + "::parse::<rndr(10:15)>")); //время показа команды от 15 до 20 секунд.
-// var cmdparam="http://geper.pythonanywhere.com<referer(" + referer + ")>"; //параметры команды
-//добавляем команду команду в презентацию. если удачно - команда появится на вкладке "Информация" программы тестирования презентации.
-// alert("prs::" + prskey + "::add::" + cmdname + "::" + cmdtime + "::" + cmdparam);
-
-var cmdname="click";
-var cmdtime=parseInt(13);
-var cmdparam="a;id;bottom_next_button;-1"
-alert("prs::" + prskey + "::add::" + cmdname + "::" + cmdtime + "::" + cmdparam); //Клик по случайной ссылки на сайте!
-
-var cmdname="click";
-var cmdtime=parseInt(9);
-var cmdparam="a;link;geper.delightfulimageparty.net;-1"
-alert("prs::" + prskey + "::add::" + cmdname + "::" + cmdtime + "::" + cmdparam); //Клик по случайной ссылки на сайте!
-
-// var cmdname="script";
-// var cmdtime=parseInt(60);
-// var cmdparam="document.getElementsByTagName('iframe').scrollIntoView()";
-// alert("prs::" + prskey + "::add::" + cmdname + "::" + cmdtime + "::" + cmdparam); // прокрутка
+//########
 var cmdname="script";
-var cmdtime=parseInt(4);
+var cmdtime=parseInt(9);
 var cmdparam="window.scrollTo(0,<rnd(3:#n)>);";
 alert("prs::" + prskey + "::add::" + cmdname + "::" + cmdtime + "::" + cmdparam); // прокрутка
-
-var cmdname="click";
-var cmdtime=parseInt(9);
-var cmdparam="a;link;geper.delightfulimageparty.net;-1"
-alert("prs::" + prskey + "::add::" + cmdname + "::" + cmdtime + "::" + cmdparam); //Клик по случайной ссылки на сайте!
-
-var cmdname="script";
-var cmdtime=parseInt(4);
-var cmdparam="window.scrollTo(0,<rnd(3:#n)>);";
-alert("prs::" + prskey + "::add::" + cmdname + "::" + cmdtime + "::" + cmdparam); // прокрутка
-
-var cmdname="click";
-var cmdtime=parseInt(9);
-var cmdparam="a;link;geper.delightfulimageparty.net;-1;"
-alert("prs::" + prskey + "::add::" + cmdname + "::" + cmdtime + "::" + cmdparam); //Клик по случайной ссылки на сайте!
-var cmdname="script";
-var cmdtime=parseInt(4);
-var cmdparam="window.scrollTo(0,<rnd(3:#n)>);";
-alert("prs::" + prskey + "::add::" + cmdname + "::" + cmdtime + "::" + cmdparam); // прокрутка
-
-var cmdname="click";
-var cmdtime=parseInt(19);
-var cmdparam="a;link;geper.delightfulimageparty.net;-1;"
-alert("prs::" + prskey + "::add::" + cmdname + "::" + cmdtime + "::" + cmdparam); //Клик по случайной ссылки на сайте!
-
-
+//########
 var cmdname="script";
 var cmdtime=parseInt(5);
+var cmdparam="window.scrollTo(0,<rnd(0:0)>);";
+alert("prs::" + prskey + "::add::" + cmdname + "::" + cmdtime + "::" + cmdparam); // прокрутка
+//########
+var cmdname="click";
+var cmdtime=parseInt(13);
+var cmdparam="img;id;skip_ad_button"
+alert("prs::" + prskey + "::add::" + cmdname + "::" + cmdtime + "::" + cmdparam); //Клик по случайной ссылки на сайте!
+//########
+var cmdname="script";
+var cmdtime=parseInt(9);
 var cmdparam="window.scrollTo(0,<rnd(3:#n)>);";
 alert("prs::" + prskey + "::add::" + cmdname + "::" + cmdtime + "::" + cmdparam); // прокрутка
+
+
 
